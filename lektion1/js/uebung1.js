ee9b7a26d487c278769d732ff52379ff528462c8
kirito274 <bro11442@googlemail.com>
2022-04-28 11:09:06 +0200

    Aufgabe 1 beendet

--- lektion1/js/uebung1.js
+++ lektion1/js/uebung1.js
@@ -10,8 +10,8 @@ let country = "Spanien"
 
 function aufgabe2(land) {
 
-  console.debug("Aufgabe 2 ==========================")
-  console.debug(country)
+    console.debug("Aufgabe 2 ==========================")
+    console.debug(country)
 }
 
 /**
@@ -24,38 +24,38 @@ let stadt, berg, fluss;
  * 4) Gebe den Variablen Werte
  */
 function aufgabe4() {
-  console.debug("Aufgabe 4 ==========================")
-  //ToDo: füge ab hier Deinen Code ein
-  stadt = "berlin"
-  berg = " Mount Everest"
-  fluss = "Nil"
-  console.debug(stadt, ", ", berg, ", ", fluss)
+    console.debug("Aufgabe 4 ==========================")
+    //ToDo: füge ab hier Deinen Code ein
+    stadt = "berlin"
+    berg = " Mount Everest"
+    fluss = "Nil"
+    console.debug(stadt, ", ", berg, ", ", fluss)
 }
 
 /**
  * 5) Erstelle die Variablen mit verschiedenem Typ
  */
 function aufgabe5() {
-  console.debug("Aufgabe 5 ==========================")
+    console.debug("Aufgabe 5 ==========================")
 
-  //ToDo: füge ab hier Deinen Code ein
-  let name = "Elon Musk"
-  let alter = 50
-  let groesseInMeter = 1.88
-  let amLeben = "true"
-  console.debug(name)
-  console.debug(alter)
-  console.debug(groesseInMeter)
-  console.debug(amLeben)
+    //ToDo: füge ab hier Deinen Code ein
+    let name = "Elon Musk"
+    let alter = 50
+    let groesseInMeter = 1.88
+    let amLeben = "true"
+    console.debug(name)
+    console.debug(alter)
+    console.debug(groesseInMeter)
+    console.debug(amLeben)
 }
 
 /**
  * Hauptfunktion, in der die Aufgaben aufgerufen werden
  */
 function main() {
-  aufgabe2()
-  aufgabe4()
-  aufgabe5()
+    aufgabe2()
+    aufgabe4()
+    aufgabe5()
 }
 
 main()
