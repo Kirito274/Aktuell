68d3100569022ddf3e47341e933219d12827c99b
kirito274 <bro11442@googlemail.com>
2022-05-02 14:54:51 +0200

    Lektion 4 fertig

--- lektion4 /js/lektion4.js
+++ lektion4 /js/lektion4.js
@@ -7,9 +7,12 @@
  * 1) 1) Erstelle eine if-Anweisung, die abfragt, ob ein bestimmter Artikel bereits gekauft wurde */
 function artikel_Gekauft() {
     //ToDo: füge ab hier deinen Code ein
-
-}
-
+  let artikelName = "Streukäse"
+   let artikelGekauft = true
+    if (artikelGekauft == true){
+        console.debug ("[Artikel]", artikelName, "wurde gekauft")
+    }
+    }
 
 /**
  * 2) Erstelle eine if-Abfrage, die in der Kosole ausgibt, dass ein Artikel hinzugefügt wurde,
@@ -17,8 +20,15 @@ function artikel_Gekauft() {
  */
 function artikel_Hinzufuegen() {
     //ToDo: füge ab hier deinen Code ein
+let gruppenName = "Getreide"
+    let artikelName = "Quinoa"
+
+    if (artikelName != "") {
+        console.debug("[gruppenName]", artikelName, "hinzugefügt")
+    }
 
 }
+
 
 /**
  * 3) Erstelle eine if-else-Anweisung, die überprüft ob die Gruppe `neueGruppe` bereits im leeren Array `gleicheGruppe`
@@ -26,6 +36,13 @@ function artikel_Hinzufuegen() {
  */
 function gruppe_Hinzufuegen() {
     // ToDo: füge ab hier deinen Code ein
+let gleicheGruppe = [];
+    let neueGruppe = "Getränke"
+    if (gleicheGruppe == 0) {
+    console.debug ("[App]", "Gruppe", neueGruppe, "hinzugefügt")}
+    else{
+        console.debug("[APP]", neueGruppe, "existiert schon!")
+    }
 
 }
 
@@ -34,7 +51,18 @@ function gruppe_Hinzufuegen() {
  */
 function einkaufsliste_Vorhanden() {
     // ToDo: füge ab hier deinen Code ein
-
+let milchProdukte = ["Milch", "Joghurt", "Sahne"]
+    let artikel = "Eisbergsalat"
+    switch(artikel){
+        case "Milch":
+        case "Joghurt":
+        case "Sahne":
+            console.debug (artikel, "ist ein Milchprodukt")
+            break
+        default:
+            console.debug("Artikel", artikel, "ist kein Milchprodukt")
+            break
+}
 }
 
 /**
@@ -44,6 +72,8 @@ function zusatz_indexOf() {
     // ToDo: füge ab hier deinen Code ein
 
 }
+
+
 
 export {
     artikel_Gekauft, artikel_Hinzufuegen, gruppe_Hinzufuegen, einkaufsliste_Vorhanden, zusatz_indexOf
