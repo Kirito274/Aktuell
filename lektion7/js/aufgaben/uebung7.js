120945681d2294ee0f1494c0884efa398b24030c
kirito274 <bro11442@googlemail.com>
2022-05-10 14:42:40 +0200

    übung 7 beendet

--- lektion7/js/aufgaben/uebung7.js
+++ lektion7/js/aufgaben/uebung7.js
@@ -16,7 +16,7 @@ function aufgabe1() {
   }
 
   //ToDo: füge ab hier deinen Code ein
-
+  let apfel = new Apfel("rot")
   console.debug(apfel.farbe)
 }
 
@@ -32,11 +32,15 @@ function aufgabe2() {
     }
 
     //ToDo: füge ab hier deinen Code ein
-  }
+    hupen() {
+console.debug("honk honk")
+    }
+}
 
   let bmw = new Auto("rot")
 
   bmw.hupen()
+
 }
 
 /**
@@ -58,6 +62,8 @@ function aufgabe3() {
   let mini = new Pkw("blau")
 
   //ToDo: füge ab hier deinen Code ein
+  console.debug(mini.farbe)
+  mini.hupen()
 }
 
 /**
@@ -68,7 +74,9 @@ function aufgabe4() {
 
   class Motorrad {
     //ToDo: füge ab hier deinen Code ein
-
+constructor(baujahr) {
+  this.baujahr = baujahr
+}
     hupen() {
       console.debug("honk honk")
     }
@@ -86,7 +94,15 @@ function aufgabe5() {
   console.debug("Aufgabe 5 ==========================");
 
   //ToDo: füge ab hier deinen Code ein
-
+class Elektroauto {
+  constructor(farbe) {
+this.farbe = farbe
+    }
+    hupen(){
+console.debug("honk honk")
+  }
+}
+let tesla = new Elektroauto("grau")
   console.debug(tesla.farbe)
   tesla.hupen()
 }
