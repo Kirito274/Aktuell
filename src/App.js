ef617ce299b192f3d246238e333aa952e1bc0357
kirito274 <bro11442@googlemail.com>
2022-05-25 15:52:09 +0200

    React fertiggestellt

--- src/App.js
+++ src/App.js
@@ -4,175 +4,188 @@ import Modell from './model/Shopping'
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.initialisieren()
-    this.state = {
-      aktiveGruppe: null,
-      showGruppenDialog: false,
-      showSortierDialog: false,
-      einkaufenAufgeklappt: true,
-      erledigtAufgeklappt: false
+    constructor(props) {
+        super(props)
+        this.initialisieren()
+        this.state = {
+            aktiveGruppe: null,
+            showGruppenDialog: false,
+            showSortierDialog: false,
+            einkaufenAufgeklappt: true,
+            erledigtAufgeklappt: false
+        }
     }
-  }
 
-  initialisieren() {
-    let Aufbau = Modell.gruppeHinzufuegen("Aufbauspiele")
-    let Aufbau2 = Aufbau.artikelHinzufuegen("Total War Warhammer")
-    Aufbau2.gekauft = true
-    let Aufbau3 = Aufbau.artikelHinzufuegen("Total War Warhammer 3")
-    Aufbau3.gekauft = false
-    let Aufbau4 = Aufbau.artikelHinzufuegen("Total War Three Kindoms Royal Edition")
-    Aufbau4.gekauft = false
-    let Aufbau5 = Aufbau.artikelHinzufuegen("Total War A Troy Saga")
-    Aufbau5.gekauft = false
-    Aufbau.artikelHinzufuegen("Total War Warhammer 2 + DLC´s")
-    let Horror = Modell.gruppeHinzufuegen("Horrorgames")
-    let Horror2 = Horror.artikelHinzufuegen("Dreadout 2")
-    Horror2.gekauft = false
-    let Horror3 = Horror.artikelHinzufuegen("Dreadout")
-    Horror3.gekauft = true
-    let Horror4 = Horror.artikelHinzufuegen("Song of Horror Complete Edition")
-    Horror4.gekauft = false
-    let Horror5 = Horror.artikelHinzufuegen("Remothered Tormented Fathers")
-    Horror5.gekauft = false
-    let game2 = Horror.artikelHinzufuegen("Elden Ring")
-    game2.gekauft = true
-    Horror.artikelHinzufuegen("Visage")
-    let Soulslike = Modell.gruppeHinzufuegen("Soulslikegames")
-    let game3 = Soulslike.artikelHinzufuegen("Nioh")
-    game3.gekauft = true
-    Soulslike.artikelHinzufuegen("Nioh 2")
-    let Soulslike2 = Soulslike.artikelHinzufuegen("Darksouls Remastered")
-    Soulslike2.gekauft = true
-    let Soulslike3 = Soulslike.artikelHinzufuegen("Darksouls 2")
-    Soulslike3.gekauft = true
-    let Soulslike4 = Soulslike.artikelHinzufuegen("Darksouls 3")
-    Soulslike4.gekauft = true
-    let Soulslike5 = Soulslike.artikelHinzufuegen("Immortal Unchained")
-    Soulslike5.gekauft = false
-    let Soulslike6 = Soulslike.artikelHinzufuegen("Salt and Santuary")
-    Soulslike6.gekauft = false
-  }
-
-
-  einkaufenAufZuKlappen() {
-    let neuerZustand = !this.state.einkaufenAufgeklappt
-    this.setState({einkaufenAufgeklappt: neuerZustand})
-  }
-
-  erledigtAufZuKlappen() {
-    let neuerZustand = !this.state.erledigtAufgeklappt
-    this.setState({erledigtAufgeklappt: neuerZustand})
-  }
-
-  // ToDo: diese Methode als 'checkHandler' an GruppenTag und ArtikelTag durchreichen
-  artikelChecken = (artikel) => {
-    // artikel.gekauft 'umpolen'
-    // 'aktion' abhängig von 'artikel.gekauft' auf "erledigt" oder "reaktiviert" setzen
-    // App.informieren mit 'aktion'
-    // 'state' aktualisieren
-  }
-
-
-  artikelHinzufuegen() {
-    // ToDo: implementiere diese Methode
-  }
-
-  setAktiveGruppe(gruppe) {
-    Modell.aktiveGruppe = gruppe
-    Modell.informieren("[App] Gruppe \"" + gruppe.name + "\" ist nun aktiv")
-    this.setState({aktiveGruppe: Modell.aktiveGruppe})
-  }
-
-  render() {
-    let nochZuKaufen = []
-    if (this.state.einkaufenAufgeklappt == true) {
-      for (const gruppe of Modell.gruppenListe) {
-        nochZuKaufen.push(<GruppenTag
-            key={gruppe.id}
-            gruppe={gruppe}
-            gekauft={false}
-            aktiv={gruppe == this.state.aktiveGruppe}
-            aktiveGruppeHandler={() => this.setAktiveGruppe(gruppe)}
-            checkHandler={this.artikelChecken}/>)
-      }
+    initialisieren() {
+        let Aufbau = Modell.gruppeHinzufuegen("Aufbauspiele")
+        let Aufbau2 = Aufbau.artikelHinzufuegen("Total War Warhammer")
+        Aufbau2.gekauft = true
+        let Aufbau3 = Aufbau.artikelHinzufuegen("Total War Warhammer 3")
+        Aufbau3.gekauft = false
+        let Aufbau4 = Aufbau.artikelHinzufuegen("Total War Three Kindoms Royal Edition")
+        Aufbau4.gekauft = false
+        let Aufbau5 = Aufbau.artikelHinzufuegen("Total War A Troy Saga")
+        Aufbau5.gekauft = false
+        Aufbau.artikelHinzufuegen("Total War Warhammer 2 + DLC´s")
+        let Horror = Modell.gruppeHinzufuegen("Horrorgames")
+        let Horror2 = Horror.artikelHinzufuegen("Dreadout 2")
+        Horror2.gekauft = false
+        let Horror3 = Horror.artikelHinzufuegen("Dreadout")
+        Horror3.gekauft = true
+        let Horror4 = Horror.artikelHinzufuegen("Song of Horror Complete Edition")
+        Horror4.gekauft = false
+        let Horror5 = Horror.artikelHinzufuegen("Remothered Tormented Fathers")
+        Horror5.gekauft = false
+        let game2 = Horror.artikelHinzufuegen("Elden Ring")
+        game2.gekauft = true
+        Horror.artikelHinzufuegen("Visage")
+        let Soulslike = Modell.gruppeHinzufuegen("Soulslikegames")
+        let game3 = Soulslike.artikelHinzufuegen("Nioh")
+        game3.gekauft = true
+        Soulslike.artikelHinzufuegen("Nioh 2")
+        let Soulslike2 = Soulslike.artikelHinzufuegen("Darksouls Remastered")
+        Soulslike2.gekauft = true
+        let Soulslike3 = Soulslike.artikelHinzufuegen("Darksouls 2")
+        Soulslike3.gekauft = true
+        let Soulslike4 = Soulslike.artikelHinzufuegen("Darksouls 3")
+        Soulslike4.gekauft = true
+        let Soulslike5 = Soulslike.artikelHinzufuegen("Immortal Unchained")
+        Soulslike5.gekauft = false
+        let Soulslike6 = Soulslike.artikelHinzufuegen("Salt and Santuary")
+        Soulslike6.gekauft = false
     }
 
 
-    let schonGekauft = []
-    if (this.state.erledigtAufgeklappt) {
-      for (const gruppe of Modell.gruppenListe) {
-        schonGekauft.push(<GruppenTag
-            key={gruppe.id}
-            gruppe={gruppe}
-            gekauft={true}
-            aktiveGruppeHandler={() => this.setAktiveGruppe(gruppe)}
-            checkHandler={this.artikelChecken}/>)
-      }
+    einkaufenAufZuKlappen() {
+        let neuerZustand = !this.state.einkaufenAufgeklappt
+        this.setState({einkaufenAufgeklappt: neuerZustand})
     }
 
-    return (
-      <div id="container">
-        {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
-        <header>
-          <h1>Gamelist</h1>
-          <label
-            className="mdc-text-field mdc-text-field--filled mdc-text-field--with-trailing-icon mdc-text-field--no-label">
-            <span className="mdc-text-field__ripple"></span>
-            <input className="mdc-text-field__input" type="search"
-                   id="artikelEingabe" placeholder="Artikel hinzufügen"
-                   onKeyPress={e => (e.key == 'Enter') ? this.artikelHinzufuegen() : ''}/>
-            <span className="mdc-line-ripple"></span>
-            <i className="material-icons mdc-text-field__icon mdc-text-field__icon--trailing"
-               tabIndex="0" role="button"
-               onClick={() => this.artikelHinzufuegen()}>add_circle</i>
-          </label>
-        </header>
-        <hr/>
+    erledigtAufZuKlappen() {
+        let neuerZustand = !this.state.erledigtAufgeklappt
+        this.setState({erledigtAufgeklappt: neuerZustand})
+    }
 
-        <main>
-          <section>
-            <h2>Noch zu kaufen
-              <i onClick={() => this.einkaufenAufZuKlappen()} className="material-icons">
-                {this.state.einkaufenAufgeklappt ? 'expand_more' : 'expand_less'}
-              </i>
-            </h2>
-            <dl>
-              {nochZuKaufen}
-            </dl>
-          </section>
-          <hr/>
-          <section>
-            <h2>Schon gekauft
-              <i onClick={() => this.erledigtAufZuKlappen()} className="material-icons">
-                {this.state.erledigtAufgeklappt ? 'expand_more' : 'expand_less'}
-              </i>
-            </h2>
-            <dl>
-              {schonGekauft}
-            </dl>
-          </section>
-        </main>
-        <hr/>
+    // ToDo: diese Methode als 'checkHandler' an GruppenTag und ArtikelTag durchreichen
+    artikelChecken = (artikel) => {
 
-        <footer>
-          <button className="mdc-button mdc-button--raised">
-            <span className="material-icons">bookmark_add</span>
-            <span className="mdc-button__ripple"></span> Gruppen
-          </button>
-          <button className="mdc-button mdc-button--raised">
-            <span className="material-icons">sort</span>
-            <span className="mdc-button__ripple"></span> Sort
-          </button>
-          <button className="mdc-button mdc-button--raised">
-            <span className="material-icons">settings</span>
-            <span className="mdc-button__ripple"></span> Setup
-          </button>
-        </footer>
-      </div>
-    )
-  }
+        artikel.gekauft = !artikel.gekauft
+        let aktion
+        if (artikel.gekauft == true) {
+            aktion = "erledigt"
+        } else {
+            aktion = "unerledigt"
+        }
+        Modell.informieren("${artikel.name} ist ${aktion)")
+        this.setState(this.state)
+
+    }
+
+    artikelHinzufuegen() {
+        // ToDo: implementiere diese Methode
+        let eingabe = document.getElementById("artikelEingabe")
+        if (eingabe.value.trim().length > 0) {
+            Modell.aktiveGruppe.artikelHinzufuegen(eingabe.value)
+            this.setState(this.state)
+        }
+        eingabe.value = ""
+        eingabe.focus()
+    }
+
+    setAktiveGruppe(gruppe) {
+        Modell.aktiveGruppe = gruppe
+        Modell.informieren("[App] Gruppe \"" + gruppe.name + "\" ist nun aktiv")
+        this.setState({aktiveGruppe: Modell.aktiveGruppe})
+    }
+
+    render() {
+        let nochZuKaufen = []
+        if (this.state.einkaufenAufgeklappt == true) {
+            for (const gruppe of Modell.gruppenListe) {
+                nochZuKaufen.push(<GruppenTag
+                    key={gruppe.id}
+                    gruppe={gruppe}
+                    gekauft={false}
+                    aktiv={gruppe == this.state.aktiveGruppe}
+                    aktiveGruppeHandler={() => this.setAktiveGruppe(gruppe)}
+                    checkHandler={this.artikelChecken}/>)
+            }
+        }
+
+
+        let schonGekauft = []
+        if (this.state.erledigtAufgeklappt) {
+            for (const gruppe of Modell.gruppenListe) {
+                schonGekauft.push(<GruppenTag
+                    key={gruppe.id}
+                    gruppe={gruppe}
+                    gekauft={true}
+                    aktiveGruppeHandler={() => this.setAktiveGruppe(gruppe)}
+                    checkHandler={this.artikelChecken}/>)
+            }
+        }
+
+        return (
+            <div id="container">
+                {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
+                <header>
+                    <h1>Gamelist</h1>
+                    <label
+                        className="mdc-text-field mdc-text-field--filled mdc-text-field--with-trailing-icon mdc-text-field--no-label">
+                        <span className="mdc-text-field__ripple"></span>
+                        <input className="mdc-text-field__input" type="search"
+                               id="artikelEingabe" placeholder="Artikel hinzufügen"
+                               onKeyPress={e => (e.key == 'Enter') ? this.artikelHinzufuegen() : ''}/>
+                        <span className="mdc-line-ripple"></span>
+                        <i className="material-icons mdc-text-field__icon mdc-text-field__icon--trailing"
+                           tabIndex="0" role="button"
+                           onClick={() => this.artikelHinzufuegen()}>add_circle</i>
+                    </label>
+                </header>
+                <hr/>
+
+                <main>
+                    <section>
+                        <h2>Noch zu kaufen
+                            <i onClick={() => this.einkaufenAufZuKlappen()} className="material-icons">
+                                {this.state.einkaufenAufgeklappt ? 'expand_more' : 'expand_less'}
+                            </i>
+                        </h2>
+                        <dl>
+                            {nochZuKaufen}
+                        </dl>
+                    </section>
+                    <hr/>
+                    <section>
+                        <h2>Schon gekauft
+                            <i onClick={() => this.erledigtAufZuKlappen()} className="material-icons">
+                                {this.state.erledigtAufgeklappt ? 'expand_more' : 'expand_less'}
+                            </i>
+                        </h2>
+                        <dl>
+                            {schonGekauft}
+                        </dl>
+                    </section>
+                </main>
+                <hr/>
+
+                <footer>
+                    <button className="mdc-button mdc-button--raised">
+                        <span className="material-icons">bookmark_add</span>
+                        <span className="mdc-button__ripple"></span> Gruppen
+                    </button>
+                    <button className="mdc-button mdc-button--raised">
+                        <span className="material-icons">sort</span>
+                        <span className="mdc-button__ripple"></span> Sort
+                    </button>
+                    <button className="mdc-button mdc-button--raised">
+                        <span className="material-icons">settings</span>
+                        <span className="mdc-button__ripple"></span> Setup
+                    </button>
+                </footer>
+            </div>
+        )
+    }
 }
 
 export default App
--- src/components/GruppenTag.js
+++ src/components/GruppenTag.js
@@ -2,43 +2,46 @@ import React from 'react'
 import ArtikelTag from './ArtikelTag'
 
 class GruppenTag extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  artikelEntfernen(name) {
-    // ToDo: implementiere diese Methode
-  }
-
-  render() {
-    const gruppe = this.props.gruppe
-
-    let gruppenHeader = ""
-    if (this.props.gekauft == false) {
-      gruppenHeader = (
-        <dt className={this.props.aktiv ? "aktiv" : "inaktiv"}
-            onClick={() => this.props.aktiveGruppeHandler(gruppe)}>
-          <span>{gruppe.name}</span>
-          <i className="material-icons">expand_less</i>
-        </dt>)
+    constructor(props) {
+        super(props)
     }
 
-    let artikelArray = []
-    for (const artikel of gruppe.artikelListe) {
-      if (artikel.gekauft == this.props.gekauft) {
-        artikelArray.push(
-          <ArtikelTag artikel={artikel} key={artikel.id}
-                      checkHandler={this.props.checkHandler}
-                      deleteHandler={() => this.artikelEntfernen(artikel.name)}/>)
-      }
+    artikelEntfernen(name) {
+        // ToDo: implementiere diese Methode
+        this.props.gruppe.artikelEntfernen(name)
+        this.props.aktiveGruppeHandler(this.props.gruppe)
     }
-    return (
-      <React.Fragment>
-        {gruppenHeader}
-        {artikelArray}
-      </React.Fragment>
-    )
-  }
+
+    render() {
+        const gruppe = this.props.gruppe
+
+        let gruppenHeader = ""
+        if (this.props.gekauft == false) {
+            gruppenHeader = (
+                <dt className={this.props.aktiv ? "aktiv" : "inaktiv"}
+                    onClick={() => this.props.aktiveGruppeHandler(gruppe)}>
+                    <span>{gruppe.name}</span>
+                    <i className="material-icons">expand_less</i>
+                </dt>)
+        }
+
+        let artikelArray = []
+        for (const artikel of gruppe.artikelListe) {
+            if (artikel.gekauft == this.props.gekauft) {
+                artikelArray.push(
+                    <ArtikelTag artikel={artikel} key={artikel.id}
+                                checkHandler={this.props.checkHandler}
+                                deleteHandler={() => this.artikelEntfernen(artikel.name)}/>)
+            }
+        }
+
+        return (
+            <React.Fragment>
+                {gruppenHeader}
+                {artikelArray}
+            </React.Fragment>
+        )
+    }
 }
 
 export default GruppenTag
